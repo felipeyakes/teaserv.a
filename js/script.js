5c2255b43b554c0f2fc29edba1eb69f955373d04
Felipe <amoabilliken@gmail.com>
2023-04-08 09:25:45 -0300

    Primer commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// Seteo de fecha de countdown.
+const countDownDate = new Date("May 20, 2023 00:01:00").getTime();
+
+// Actualizar el contador cada 1s.
+const x = setInterval(function() {
+
+  // Encontrar el day, hour y minute de ahora según el dispositivo del usuario.
+const now = new Date().getTime();
+
+  // Encotnrar la distancia entre hoy y el day, hours and minutes seteado.
+const distance = countDownDate - now;
+
+  // Calcular days, hours and minutes.
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+  // Mostrar los days, hours and minutes en el HTML.
+document.getElementById("days").innerHTML = days;
+document.getElementById("hours").innerHTML = hours;
+document.getElementById("minutes").innerHTML = minutes;
+
+  // Mensaje al terminar el countdown date.
+if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("days").innerHTML = 0;
+    document.getElementById("hours").innerHTML = 0;
+    document.getElementById("minutes").innerHTML = 0;
+    alert("Tripulación, próximos al despegue.");
+}
+}, 1000);
